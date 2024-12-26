24b934380500db44a853f898de70e2649bede71f
EngJao89 <eng_joao@outlook.com>
2024-12-26 11:42:14 -0300

    implements alerts of notifications snaps

---
+++ SnapchatCloneApp/Alerta.swift
@@ -0,0 +1,29 @@
+//
+//  Alerta.swift
+//  SnapchatCloneApp
+//
+//  Created by João Ricardo Martins Ribeiro on 26/12/24.
+//
+
+import UIKit
+
+class Alerta {
+    
+    var titulo: String
+    var mensagem: String
+    
+    init(titulo: String, mensagem: String) {
+        self.titulo = titulo
+        self.mensagem = mensagem
+    }
+    
+    func getAlerta() -> UIAlertController {
+        
+        let alerta = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+        let acaoCancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alerta.addAction( acaoCancelar )
+        return alerta
+    }
+
+}
